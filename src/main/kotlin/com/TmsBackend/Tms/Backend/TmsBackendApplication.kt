64bf2ef74b0e55d08d55fcb39792a3d23891849d
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-12-25 12:01:26 +0530

    first commit

---
+++ src/main/kotlin/com/TmsBackend/Tms/Backend/TmsBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.TmsBackend.Tms.Backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TmsBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<TmsBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/TmsBackend/Tms/Backend/TmsBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.TmsBackend.Tms.Backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TmsBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
